2ae2b8b6142c77315431c644d68a21eff7f7e089
yanweichen <yanweichen@live.com>
2017-04-03 23:52:20 +0800

    feat:整合spring，springMVC，MyBatis

---
+++ core/src/main/java/com/ywc/mgt/user/dao/UserMapper.java
@@ -0,0 +1,7 @@
+package com.ywc.mgt.user.dao;
+
+import com.ywc.mgt.user.model.User;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface UserMapper extends Mapper<User> {
+}
---
+++ core/src/main/java/com/ywc/mgt/user/service/impl/UserServiceImpl.java
@@ -0,0 +1,34 @@
+package com.ywc.mgt.user.service.impl;
+
+import com.ywc.mgt.user.dao.UserMapper;
+import com.ywc.mgt.user.model.User;
+import com.ywc.mgt.user.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * Created by yanweichen on 2017/4/3.
+ */
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Override
+    public User getUser() {
+        return userMapper.selectByPrimaryKey(1);
+    }
+
+    @Override
+    @Transactional
+    public void addUser() {
+        User user = new User();
+        user.setUserName("asdasd");
+        user.setUserPassword("sadasdasdasdas");
+        userMapper.insert(user);
+        throw new RuntimeException();
+    }
+
+}
