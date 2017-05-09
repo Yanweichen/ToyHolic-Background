90306bfb10ab9e15d6eb6867aec95b1621df9c00
yanweichen <505717760@qq.com>
2017-05-09 11:09:50 +0800

    feat:测试文件生成

--- core/src/main/java/com/ywc/mgt/user/model/User.java
+++ core/src/main/java/com/ywc/mgt/user/model/User.java
@@ -8,19 +8,31 @@ public class User {
      * 用户id
      */
     @Id
-    @Column(name = "user_id")
+    @Column(name = "USER_ID")
     private Integer userId;
 
-    @Column(name = "user_name")
+    /**
+     * 用户账号
+     */
+    @Column(name = "USER_ACCOUNT")
+    private String userAccount;
+
+    /**
+     * 用户姓名
+     */
+    @Column(name = "USER_NAME")
     private String userName;
 
-    @Column(name = "user_password")
+    /**
+     * 用户密码
+     */
+    @Column(name = "USER_PASSWORD")
     private String userPassword;
 
     /**
      * 获取用户id
      *
-     * @return user_id - 用户id
+     * @return USER_ID - 用户id
      */
     public Integer getUserId() {
         return userId;
@@ -36,28 +48,54 @@ public class User {
     }
 
     /**
-     * @return user_name
+     * 获取用户账号
+     *
+     * @return USER_ACCOUNT - 用户账号
+     */
+    public String getUserAccount() {
+        return userAccount;
+    }
+
+    /**
+     * 设置用户账号
+     *
+     * @param userAccount 用户账号
+     */
+    public void setUserAccount(String userAccount) {
+        this.userAccount = userAccount;
+    }
+
+    /**
+     * 获取用户姓名
+     *
+     * @return USER_NAME - 用户姓名
      */
     public String getUserName() {
         return userName;
     }
 
     /**
-     * @param userName
+     * 设置用户姓名
+     *
+     * @param userName 用户姓名
      */
     public void setUserName(String userName) {
         this.userName = userName;
     }
 
     /**
-     * @return user_password
+     * 获取用户密码
+     *
+     * @return USER_PASSWORD - 用户密码
      */
     public String getUserPassword() {
         return userPassword;
     }
 
     /**
-     * @param userPassword
+     * 设置用户密码
+     *
+     * @param userPassword 用户密码
      */
     public void setUserPassword(String userPassword) {
         this.userPassword = userPassword;
